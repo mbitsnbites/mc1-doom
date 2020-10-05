89fe0e35208e926695d3f5423b6dd72c8a578745
m <m@bitsnbites.eu>
2020-10-05 20:55:41 +0200

    z_zone: Fix gcc indentation warning

--- src/z_zone.h
+++ src/z_zone.h
@@ -69,14 +69,15 @@ typedef struct memblock_s
 // This is used to get the local FILE:LINE info from CPP
 // prior to really call the function in question.
 //
-#define Z_ChangeTag(p,t) \
-{ \
-      if (( (memblock_t *)( (byte *)(p) - sizeof(memblock_t)))->id!=0x1d4a11) \
-	  I_Error("Z_CT at "__FILE__":%i",__LINE__); \
-	  Z_ChangeTag2(p,t); \
-};
-
-
+#define Z_ChangeTag(p, t)                                                      \
+    {                                                                          \
+	if (((memblock_t*)((byte*)(p) - sizeof (memblock_t)))->id != 0x1d4a11) \
+	    I_Error (                                                          \
+		"Z_CT at "__FILE__                                             \
+		":%i",                                                         \
+		__LINE__);                                                     \
+	Z_ChangeTag2 (p, t);                                                   \
+    };
 
 #endif
 //-----------------------------------------------------------------------------
