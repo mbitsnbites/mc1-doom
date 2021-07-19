5884f1929c78d8915f818cb74c8ad6e7ecbf9cb9
m <m@bitsnbites.eu>
2021-07-19 19:21:37 +0200

    run.sh: Make profiling optional

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,11 @@
-mrisc32-elf-readelf -sW out/mc1doom | grep FUNC | awk '{print $2,$8}' > /tmp/symbols.csv
-mr32sim -g -ga 1073744608 -gp 1073743556 -gd 8 -gw 320 -gh 180 -P /tmp/symbols.csv "$@" out/mc1doom.bin
+#!/bin/bash
+
+PROFILE_ARGS=""
+if [ "$1" == "--profile" ] ; then
+    shift 1
+    mrisc32-elf-readelf -sW out/mc1doom | grep FUNC | awk '{print $2,$8}' > /tmp/symbols.csv
+    PROFILE_ARGS="-P /tmp/symbols.csv"
+fi
+
+mr32sim -g -ga 1073744608 -gp 1073743556 -gd 8 -gw 320 -gh 180 ${PROFILE_ARGS} "$@" out/mc1doom.bin
 
