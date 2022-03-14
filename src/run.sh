e7b55b0af1b2279c397bd0907141963fccc7c7b6
m <m@bitsnbites.eu>
2022-03-14 19:22:48 +0100

    run.sh: Misc script improvements

--- src/run.sh
+++ src/run.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
-PROFILE_ARGS=""
-if [ "$1" == "--profile" ] ; then
-    shift 1
-    mrisc32-elf-readelf -sW out/mc1doom | grep FUNC | awk '{print $2,$8}' > /tmp/symbols.csv
-    PROFILE_ARGS="-P /tmp/symbols.csv"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+pushd "${SCRIPT_DIR}" > /dev/null
+
+EXE_FILE=out/mc1doom
+PROFILE_FILE=""
+SIM_ARGS=""
+
+# Collect script arguments.
+while true ; do
+    if [ "$1" == "--profile" ] ; then
+        shift 1
+        PROFILE_FILE=/tmp/mr32sim-sym-$$
+        mrisc32-elf-readelf -sW "${EXE_FILE}" | grep FUNC | awk '{print $2,$8}' > "${PROFILE_FILE}"
+        SIM_ARGS="${SIM_ARGS} -P ${PROFILE_FILE}"
+    elif [ "$1" == "-f" ] ; then
+        shift 1
+        SIM_ARGS="${SIM_ARGS} -f"
+    else
+        break
+    fi
+done
+
+# Run the executable in the simulator.
+mr32sim -g -ga 0x40000ae0 -gp 0x400006c4 -gd 8 -gw 320 -gh 180 ${SIM_ARGS} "${EXE_FILE}" "$@"
+
+# Delete the temporary profiling data.
+if [ -n "${PROFILE_FILE}" ] ; then
+    rm "${PROFILE_FILE}"
 fi
 
-mr32sim -g -ga 0x40000ae0 -gp 0x400006c4 -gd 8 -gw 320 -gh 180 ${PROFILE_ARGS} "$@" out/mc1doom
+popd > /dev/null
+
